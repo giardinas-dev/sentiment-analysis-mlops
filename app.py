c98a20ed9d2e705a03ed8a643d2a7ac0d48f7d93
Sergio Giardina <giardinasergio3@gmail.com>
2025-08-05 12:49:22 +0200

    fix app.py

--- app.py
+++ app.py
@@ -3,25 +3,15 @@ from src.utils import SentimentAnalyzer
 import torch
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
-# Configurazioni modello (modifica in base al modello che usi)
-MODEL_NAME = "nlptown/bert-base-multilingual-uncased-sentiment"
 
-# Inizializzo tokenizer e modello
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
 
-# Config dummy per id2label (dipende dal modello, qui es. 5 classi)
-config = type('Config', (), {})()  # creo un oggetto vuoto
-config.id2label = {
-    0: "1 star",
-    1: "2 stars",
-    2: "3 stars",
-    3: "4 stars",
-    4: "5 stars",
-}
+MODEL = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+config = AutoConfig.from_pretrained(MODEL)
 
 # Istanza del sentiment analyzer
 analyzer = SentimentAnalyzer(model, tokenizer, config)
+       
 
 def analyze_sentiment(text):
     results = analyzer.predict(text)
@@ -31,7 +21,7 @@ def analyze_sentiment(text):
 iface = gr.Interface(
     fn=analyze_sentiment,
     inputs=gr.Textbox(lines=2, placeholder="Inserisci testo da analizzare..."),
-    outputs=gr.Label(num_top_classes=5),
+    outputs=gr.Label(num_top_classes=3),
     title="Sentiment Analyzer Twitter",
     description="Analizza il sentiment del testo inserito usando un modello BERT multilingue."
 )
